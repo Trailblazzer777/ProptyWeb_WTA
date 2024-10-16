bfb1f0f2ee53ddf49968be7f14a636a118cf20cc
Vinay R <vinayraj24072003@gmail.com>
2024-10-16 18:58:41 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function runAnimations() {
+  document
+    .querySelector("#id-1272005")
+    .addEventListener("click", func_1272005_127555);
+
+  function func_1272005_127555(e) {
+    document
+      .querySelector(".parent-div")
+      .classList.add("parent-animation-1271515");
+
+  }
+}
+runAnimations();
